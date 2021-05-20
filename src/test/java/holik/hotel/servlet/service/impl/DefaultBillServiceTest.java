c4e1afd84f4348965e635727df01bebcccb428a9
Vlad Golik <moyfolder@gmail.com>
2021-05-20 20:47:38 +0300

    Bill service tests

---
+++ src/test/java/holik/hotel/servlet/service/impl/DefaultBillServiceTest.java
@@ -0,0 +1,105 @@
+package holik.hotel.servlet.service.impl;
+
+import holik.hotel.servlet.repository.model.Application;
+import holik.hotel.servlet.repository.model.Bill;
+import holik.hotel.servlet.repository.model.Room;
+import holik.hotel.servlet.service.ApplicationService;
+import holik.hotel.servlet.service.BillService;
+import holik.hotel.servlet.service.RoomService;
+import org.checkerframework.checker.units.qual.A;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class DefaultBillServiceTest {
+    private ApplicationService applicationService;
+    private RoomService roomService;
+    private BillService billService;
+
+    @Before
+    public void setUp() {
+        roomService = mock(RoomService.class);
+        applicationService = mock(ApplicationService.class);
+        billService = new DefaultBillService(applicationService, roomService);
+    }
+
+    @Test
+    public void getUserBills() {
+        List<Application> bookedApplications = new ArrayList<>();
+        Application firstApplication = new Application();
+        Application secondApplication = new Application();
+        Application thirdApplication = new Application();
+
+        LocalDateTime now = LocalDateTime.now();
+        firstApplication.setDatetimeOfArrival(now);
+        secondApplication.setDatetimeOfArrival(now);
+        thirdApplication.setDatetimeOfArrival(now);
+
+        firstApplication.setDatetimeOfLeaving(now.plusDays(1));
+        secondApplication.setDatetimeOfLeaving(now.plusDays(2));
+        thirdApplication.setDatetimeOfLeaving(now.plusDays(3));
+
+        firstApplication.setRoomId(1);
+        secondApplication.setRoomId(2);
+        thirdApplication.setRoomId(3);
+
+        bookedApplications.add(firstApplication);
+        bookedApplications.add(secondApplication);
+        bookedApplications.add(thirdApplication);
+
+        Room firstRoom = new Room();
+        Room secondRoom = new Room();
+        Room thirdRoom = new Room();
+
+        firstRoom.setId(1);
+        secondRoom.setId(2);
+        thirdRoom.setId(3);
+
+        firstRoom.setPrice(2);
+        secondRoom.setPrice(3);
+        thirdRoom.setPrice(4);
+
+        when(roomService.getRoomById(1)).thenReturn(Optional.of(firstRoom));
+        when(roomService.getRoomById(2)).thenReturn(Optional.of(secondRoom));
+        when(roomService.getRoomById(3)).thenReturn(Optional.of(thirdRoom));
+        when(applicationService.getBookedApplicationsByUserId(5)).thenReturn(bookedApplications);
+
+        List<Bill> bills = billService.getUserBills(5);
+
+        verify(roomService).getRoomById(1);
+        verify(roomService).getRoomById(2);
+        verify(roomService).getRoomById(3);
+        verify(applicationService).getBookedApplicationsByUserId(5);
+
+        Bill firstBill = bills.get(0);
+        Bill secondBill = bills.get(1);
+        Bill thirdBill = bills.get(2);
+
+        assertEquals(3, bills.size());
+        assertEquals(firstApplication, firstBill.getApplication());
+        assertEquals(secondApplication, secondBill.getApplication());
+        assertEquals(thirdApplication, thirdBill.getApplication());
+
+        assertEquals(1, firstBill.getRoom().getId());
+        assertEquals(2, secondBill.getRoom().getId());
+        assertEquals(3, thirdBill.getRoom().getId());
+
+        assertEquals(2 * 24, firstBill.getPrice(), 0.0);
+        assertEquals(3 * 24 * 2, secondBill.getPrice(), 0.0);
+        assertEquals(4 * 24 * 3, thirdBill.getPrice(), 0.0);
+    }
+
+    @Test
+    public void getUserBillsWhenNoBills() {
+        when(applicationService.getBookedApplicationsByUserId(2)).thenReturn(new ArrayList<>());
+        List<Bill> bills = billService.getUserBills(2);
+        assertEquals(0, bills.size());
+    }
+}
