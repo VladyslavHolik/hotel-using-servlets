07c9233bd74c7156f2896ad97dc5f508c26334a2
Vlad Golik <moyfolder@gmail.com>
2021-05-20 21:41:34 +0300

    UserService tests

---
+++ src/test/java/holik/hotel/servlet/service/impl/DefaultUserServiceTest.java
@@ -0,0 +1,83 @@
+package holik.hotel.servlet.service.impl;
+
+import holik.hotel.servlet.repository.UserRepository;
+import holik.hotel.servlet.repository.model.User;
+import holik.hotel.servlet.service.EncoderService;
+import holik.hotel.servlet.service.UserService;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class DefaultUserServiceTest {
+    private UserRepository userRepository;
+    private EncoderService encoderService;
+    private UserService userService;
+
+    @Before
+    public void setUp() throws Exception {
+        userRepository = mock(UserRepository.class);
+        encoderService = mock(EncoderService.class);
+        userService = new DefaultUserService(userRepository, encoderService);
+    }
+
+    @Test
+    public void createUser() {
+        User user = new User();
+        userService.createUser(user);
+        verify(userRepository).createUser(user);
+    }
+
+    @Test
+    public void getUserById() {
+        userService.getUserById(2);
+        verify(userRepository).getUserById(2);
+    }
+
+    @Test
+    public void getUserByEmail() {
+        userService.getUserByEmail("email");
+        verify(userRepository).getUserByEmail("email");
+    }
+
+    @Test
+    public void areCredentialsTrueIfTrue() {
+        User user = new User();
+        String salt = "salt";
+        String hash = "hash";
+        user.setSalt(salt);
+        user.setPasswordHash(hash);
+
+        when(userRepository.getUserByEmail("email")).thenReturn(Optional.of(user));
+        when(encoderService.areHashesEqual(salt, hash, "password")).thenReturn(true);
+
+        boolean result = userService.areCredentialsTrue("email", "password");
+        assertTrue(result);
+    }
+
+    @Test
+    public void areCredentialsTrueIfFalseCase1() {
+        User user = new User();
+        String salt = "salt";
+        String hash = "hash";
+        user.setSalt(salt);
+        user.setPasswordHash(hash);
+
+        when(userRepository.getUserByEmail("email")).thenReturn(Optional.of(user));
+        when(encoderService.areHashesEqual(salt, hash, "password")).thenReturn(false);
+
+        boolean result = userService.areCredentialsTrue("email", "password");
+        assertFalse(result);
+    }
+
+    @Test
+    public void areCredentialsTrueIfFalseCase2() {
+        when(userRepository.getUserByEmail("email")).thenReturn(Optional.empty());
+
+        boolean result = userService.areCredentialsTrue("email", "password");
+        assertFalse(result);
+    }
+}
